8a22dd9482f1727c442a598ebd26d89c311899a0
jburgoon1 <77408299+jburgoon1@users.noreply.github.com>
2021-10-07 11:46:43 -0600

    finished

---
+++ count-zeroes.js
@@ -0,0 +1,35 @@
+function countZeroes(arr)
+{
+      
+    var first = firstZero(arr);
+ 
+     if (first === -1)
+         return 0;
+
+     return (arr.length - first);
+  
+    function firstZero(arr, low=0, high=arr.length-1) {
+        if (high >= low) {
+         
+          
+            let mid =  low+Math.floor((high-low)/2);
+            if ((mid == 0 || arr[mid - 1] == 1) && arr[mid] == 0)
+                return mid;
+ 
+            if (arr[mid] == 1) 
+                return firstZero(arr, mid + 1, high);
+            else 
+                return firstZero(arr, low, mid - 1);
+        }
+        return -1;
+    }
+ 
+    
+  
+     
+      
+    }
+ 
+
+
+module.exports = countZeroes
